3342102b9bfaf1c0c55e336d9837f99cadaf77c8
Robin Dyment <T00600907@mytru.ca>
2023-10-19 18:42:34 -0700

    ruh roh

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Goodbye, Planet!");
+        System.out.println("Have a nice day, Planet!");
     }
 }
